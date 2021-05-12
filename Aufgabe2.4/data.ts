80318d03a5c850b8aff038f99c802720d928045a
Jochem Punt <jochem.punt@gmail.com>
2021-05-12 17:44:02 +0200

    hä dateinamen werden in github aufeinmal kleingeschrieben

--- Aufgabe2.4/data.ts
+++ Aufgabe2.4/data.ts
@@ -31,15 +31,15 @@ namespace aufgabe2_4 {
     }
 
 
-    let data: Data = {broetchen:    [{name: "Weißbroetchen", preis: 0.6, darstellung: "darstellungen/BurgerBodenHell.png", darstellung2: "darstellungen/BurgerDach.png"},
-                                     {name: "Dinkelbroetchen", preis: 0.7, darstellung: "darstellungen/BurgerBodenMittel.png", darstellung2: "darstellungen/BurgerDachMittel.png"},
-                                     {name: "Volkornbroetchen", preis: 0.8, darstellung: "darstellungen/BurgerBodenDunkel.png", darstellung2: "darstellungen/BurgerDachDunkel.png"}],
+    let data: Data = {broetchen:    [{name: "Weißbroetchen", preis: 0.6, darstellung: "darstellungen/burgerBodenHell.png", darstellung2: "darstellungen/burgerDach.png"},
+                                     {name: "Dinkelbroetchen", preis: 0.7, darstellung: "darstellungen/burgerBodenMittel.png", darstellung2: "darstellungen/burgerDachMittel.png"},
+                                     {name: "Volkornbroetchen", preis: 0.8, darstellung: "darstellungen/burgerBodenDunkel.png", darstellung2: "darstellungen/burgerDachDunkel.png"}],
                             zutaten: [{name: "tomaten", preis: 0.45, darstellung: "darstellungen/Tomaten.png"},
                                      {name: "salat", preis: 0.4, darstellung: "darstellungen/Salat.png"},
                                      {name: "käse", preis: 0.65, darstellung: "darstellungen/Käse.png"}] , 
-                            pattys: [{name: "RinderPatty", preis: 1.8, darstellung: "darstellungen/BurgerPattyRind.png", vegetarisch: false},
-                                     {name: "HünchenPatty" , preis: 1.5, darstellung: "darstellungen/BurgerPattyChicken.png", vegetarisch: false},
-                                     {name: "VeggiePatty" , preis: 1.7, darstellung: "darstellungen/BurgerPattyVeggy.png", vegetarisch: true}] };
+                            pattys: [{name: "RinderPatty", preis: 1.8, darstellung: "darstellungen/burgerPattyRind.png", vegetarisch: false},
+                                     {name: "HünchenPatty" , preis: 1.5, darstellung: "darstellungen/burgerPattyChicken.png", vegetarisch: false},
+                                     {name: "VeggiePatty" , preis: 1.7, darstellung: "darstellungen/burgerPattyVeggy.png", vegetarisch: true}] };
 
     export let daten: string = JSON.stringify(data);
 
